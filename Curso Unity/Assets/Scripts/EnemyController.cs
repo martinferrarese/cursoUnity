6be6375f39e731b9fbb80e86452ab4da08834d22
Ferra <martin.ferrarese@gmail.com>
2020-11-22 15:56:04 -0300

    Agrega un enemigo que se mueve a la pantalla

---
+++ Curso Unity/Assets/Scripts/EnemyController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    public float velocidad = 2f;
+    public Rigidbody2D enemigo;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        enemigo = GetComponent<Rigidbody2D>();
+        enemigo.velocity = Vector2.left * velocidad;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
